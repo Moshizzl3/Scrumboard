58787fc8d21ebffa04bc221a44f20e4fac77372b
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-09-14 20:15:42 +0200

    added get requests and delete requests

--- routes/task.js
+++ routes/task.js
@@ -3,11 +3,19 @@ import { dbConnection } from "../data/db.js";
 
 export const taskRouter = Router();
 
-taskRouter.get("/tasks", (req, res) => {
-  res.send("heeej");
+taskRouter.get("/tasks", async (req, res) => {
+  const result = await dbConnection.promise().query(`SELECT * FROM TASK`);
+  const tasks = result[0];
+  res.send(tasks);
 });
 
-taskRouter.get("/tasks/:id", (req, res) => {});
+taskRouter.get("/tasks/:id", async (req, res) => {
+  const result = await dbConnection
+    .promise()
+    .query(`SELECT * FROM TASK WHERE task_id = ${req.params.id}`);
+  const task = result[0];
+  res.send(task);
+});
 
 taskRouter.post("/tasks", (req, res) => {
   const body = { ...req.body };
@@ -19,4 +27,9 @@ taskRouter.post("/tasks", (req, res) => {
 
 taskRouter.patch("/tasks/:id", (req, res) => {});
 
-taskRouter.delete("/tasks/:id", (req, res) => {});
+taskRouter.delete("/tasks/:id", (req, res) => {
+  dbConnection
+    .promise()
+    .query(`DELETE FROM TASK WHERE task_id = ${req.params.id}`);
+  res.send(`Task with id: ${req.params.id} is now deleted`);
+});
