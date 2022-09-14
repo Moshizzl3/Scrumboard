417b398188fc5750f31994a093ff633978e5d2ad
Mohamad Ibrahim <mohamedibr@gmail.com>
2022-09-14 19:58:07 +0200

    can now post to db

--- routes/task.js
+++ routes/task.js
@@ -3,27 +3,20 @@ import { dbConnection } from "../data/db.js";
 
 export const taskRouter = Router();
 
+taskRouter.get("/tasks", (req, res) => {
+  res.send("heeej");
+});
 
-taskRouter.get("/tasks", (req, res) =>{
-    res.send("heeej")
-})
+taskRouter.get("/tasks/:id", (req, res) => {});
 
-taskRouter.get("/tasks/:id", (req, res) =>{
+taskRouter.post("/tasks", (req, res) => {
+  const body = { ...req.body };
+  dbConnection.promise()
+    .query(`INSERT INTO TASK (creation_date, description, estimated_time, name, status, time_spent, user_story_id, user_id) 
+            VALUES('${body.creationDate}', '${body.description}', ${body.estimatedTime}, '${body.name}','${body.status}', 
+            ${body.timeSpent}, ${body.userStoryId}, ${body.userId})`);
+});
 
-})
+taskRouter.patch("/tasks/:id", (req, res) => {});
 
-taskRouter.post("/tasks", (req, res) =>{
-
-    const body = {...req.body}
-
-    dbConnection.promise().query(`insert into task (name, description,estimated_time, time_spent) values('${body.name}', '${body.description}', 0, 0)`)
-
-})
-
-taskRouter.patch("/tasks/:id", (req, res) =>{
-
-})
-
-taskRouter.delete("/tasks/:id", (req, res) =>{
-
-})
+taskRouter.delete("/tasks/:id", (req, res) => {});
